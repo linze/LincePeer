7ad50523d8c1a637ae63a40bdd8401a313f3f863
Francisco Canela <darkydelphi@gmail.com>
2010-05-31 20:28:16 +0200

    Fake branch used to avoid students access de code. Done by teachers petition

--- gsyc/compilar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "[*] Lanzando "gnatmake" sobre "lince.adb"  suponiendo librerias en "/opt/ll/lib"."
-gnatmake -I/opt/ll/lib lince.adb
-gnatmake -I/opt/ll/lib lince.adb
-echo "[*] Limpiando la casa."
-rm *.ali
-rm *.o
-echo "[*] Fin del proceso. Aprende a usar makefile algún día, carapan ;)"
-
